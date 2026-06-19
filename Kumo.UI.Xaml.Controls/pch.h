2a5fb2f76055b2b254af4ac03a2138312dab7ea2
k1mlka <k1mlka@outlook.com>
2026-06-19 23:20:07 +0800

    添加项目文件。

---
+++ Kumo.UI.Xaml.Controls/pch.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+#include <windows.h>
+#include <unknwn.h>
+#include <restrictederrorinfo.h>
+#include <hstring.h>
+
+// Undefine GetCurrentTime macro to prevent
+// conflict with Storyboard::GetCurrentTime
+#undef GetCurrentTime
+
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.Foundation.Collections.h>
