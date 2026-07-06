025e4d82e1105ed0f97a1ed601a3f3a7ef691562
k1mlka <k1mlka@outlook.com>
2026-07-07 03:07:43 +0800

    Tests: iuxc:ScrollView with muxc:ItemsRepeater

--- Tests/CoreIslandApp1/MainPage.xaml.cs
+++ Tests/CoreIslandApp1/MainPage.xaml.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
@@ -5,19 +6,38 @@ namespace CoreIslandApp1
 {
     public sealed partial class MainPage : Page
     {
+        private sealed partial class StringItemsSource
+            : List<string>, Microsoft.UI.Xaml.Controls.IKeyIndexMapping
+        {
+            public string KeyFromIndex(int index) => this[index];
+            public int IndexFromKey(string key)
+            {
+                int i = IndexOf(key);
+                return i >= 0 ? i : -1;
+            }
+        }
+
+        private readonly StringItemsSource _items = new()
+        {
+            "sadfsdf", "sadfsdf", "sadfsdf", "sadfsdf", "sadfsdf",
+            "sadfsdf", "sadfsdf", "sadfsdf", "sadfsdf", "sadfsdf",
+            "sadfsdf", "sadfsdf", "sadfsdf", "sadfsdf", "sadfsdf",
+            "sadfsdf", "sadfsdf", "sadfsdf", "sadfsdf", "sadfsdf",
+            "sadfsdf", "sadfsdf", "sadfsdf", "sadfsdf", "sadfsdf",
+            "sadfsdf", "sadfsdf", "sadfsdf", "sadfsdf", "sadfsdf",
+            "sadfsdf", "sadfsdf", "sadfsdf", "sadfsdf",
+        };
+
         public MainPage()
         {
             InitializeComponent();
+            repeater.ItemsSource = _items;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            // Open a new CoreIsland window on button click
             var rootFrame = new Frame();
-            var newWindow = new CoreIsland.Window()
-            {
-                Content = rootFrame
-            };
+            var newWindow = new CoreIsland.Window() { Content = rootFrame };
             rootFrame.Navigate(typeof(MainPage));
             newWindow.Title = "CoreIsland Window";
             newWindow.Activate();
